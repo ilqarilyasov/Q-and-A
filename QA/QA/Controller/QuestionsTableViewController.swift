aae0aef8d034c0ed1b94b2c6f15c83011984da1b
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-04 08:56:29 -0500

    QuestionsTableViewController - added necessary functions. Implemented cellForRowAt method.

--- QA/QA/Controller/QuestionsTableViewController.swift
+++ QA/QA/Controller/QuestionsTableViewController.swift
@@ -9,34 +9,27 @@
 import UIKit
 
 class QuestionsTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return questionController.questions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "QACellSegue", for: indexPath) as? QuestionsTableViewCell else { return UITableViewCell()}
+        
+        let theQuestion = questionController.questions[indexPath.row]
+        cell.question = theQuestion
+        
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -46,9 +39,7 @@ class QuestionsTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -56,7 +47,9 @@ class QuestionsTableViewController: UITableViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
+    let questionController = QuestionController()
+    
     @IBAction func askQuestionAction(_ sender: Any) {
         
     }
